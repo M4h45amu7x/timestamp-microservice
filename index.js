a5186c526dc6f0e70c2d70cf77ec718fc8636225
M4h45amu7x <lovelovethekiss123@gmail.com>
2022-11-25 16:03:02 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+const cors = require('cors')
+app.use(
+	cors({
+		optionsSuccessStatus: 200,
+	}),
+)
+
+app.get('/api/:date?', (req, res) => {
+	try {
+		const date = req.params.date ? Number(req.params.date) : new Date().getTime()
+
+		if (date) {
+			return res.send({
+				unix: date,
+				utc: new Date(date).toUTCString(),
+			})
+		}
+	} catch (error) {}
+
+	res.send({
+		error: 'Invalid Date',
+	})
+})
+
+app.listen(3000)
+
+module.exports = app
